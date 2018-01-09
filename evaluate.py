532eca2fb80ef69aebdcf2d4f982ed79c50ffd1d
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2018-01-08 22:50:33 -0500

    modify evaluate.py to calc AUC

--- evaluate.py
+++ evaluate.py
@@ -2,46 +2,45 @@ from keras.models import load_model
 import numpy as np
 from citeulikeDataPrep import getTrainTestData
 
-trainSet, devSet, testSet = getTrainTestData()
-del trainSet
-input0_dev = np.array([ d[0] for d in devSet ], dtype='int32')
-input1_dev = np.array([ d[1] for d in devSet ])
-output_dev = np.array([ d[2] for d in devSet ])
-input0_dev = np.reshape(input0_dev, (-1,1))
-output_dev = np.reshape(output_dev, (-1,1))
+input0_devf = open("input0_devf.npy", 'rb')
+input1_devf = open("input1_devf.npy", 'rb')
+output_devf = open("output_devf.npy", 'rb')
+input0_testf = open("input0_testf.npy", 'rb')
+input1_testf = open("input1_testf.npy", 'rb')
+output_testf = open("output_testf.npy", 'rb')
+
+input0_dev = np.load(input0_devf)
+input1_dev = np.load(input1_devf)
+output_dev = np.load(output_devf)
 
 model = load_model('tmp-out.h5')
 pred = model.predict([input0_dev, input1_dev])
 
-"""
-trainSet, devSet, testSet = getTrainTestData()
-input0_train = np.array([ d[0] for d in trainSet[:10000] ], dtype='int32')
-input1_train = np.array([ d[1] for d in trainSet[:10000] ])
-output_train = np.array([ d[2] for d in trainSet[:10000] ])
-input0_train = np.reshape(input0_train, (-1,1))
-output_train = np.reshape(output_train, (-1,1))
-
-input0_dev = input0_train
-input1_dev = input1_train
-output_dev = output_train
-
-model = load_model('tmp-out.h5')
-pred = model.predict([input0_dev, input1_dev])
-"""
-
-p0r0 = 0
-p1r0 = 0
-p0r1 = 0
-p1r1 = 0
-for i in range(10000):
-    if output_dev[i][0] > 0.5:
-        if pred[0][i][0] > 0.1:
-            p1r1 += 1
+threshold = 0.00
+X = [0.0] # false positive rate
+Y = [0.0] # true positive rate
+while threshold < 1.00:
+    threshold += 0.01
+    p0r0 = 0
+    p1r0 = 0
+    p0r1 = 0
+    p1r1 = 0
+    for i in range(11102):
+        if output_dev[i][0] > 0.5:
+            if pred[0][i][0] > 1.00 - threshold:
+                p1r1 += 1
+            else:
+                p0r1 += 1
         else:
-            p0r1 += 1
-    else:
-        if pred[0][i][0] > 0.1:
-            p1r0 += 1
-        else:
-            p0r0 += 1
+            if pred[0][i][0] > 1.00 - threshold:
+                p1r0 += 1
+            else:
+                p0r0 += 1
+    X.append( p1r0 / (p1r0 + p0r0) )
+    Y.append( p1r1 / (p1r1 + p0r1) )
+
+AUC = 0.0
+for i in range(1, len(X)):
+    AUC += (Y[i-1] + Y[i]) * (X[i] - X[i-1])
+print("AUC: " + str(AUC/2.0))
 
